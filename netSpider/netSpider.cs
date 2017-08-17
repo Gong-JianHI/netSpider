b9b11fdb33dfe847488d6f76f2ab87db96c4f7ad
Gong-JianHI <1365654168@qq.com>
2017-08-17 10:27:53 +0800

    project

---
+++ netSpider/netSpider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace netSpider
+{
+    public class netSpider
+    {
+       
+        public Stream  getScode(string URL, bool keepAlive, int timeOut, string method, string accept, string userAgent)
+        {
+            HttpWebRequest mywr = (HttpWebRequest)WebRequest.Create(URL);
+            mywr.KeepAlive = keepAlive;
+            mywr.Timeout = timeOut;
+            mywr.Method = method;
+            mywr.Accept = accept;
+            mywr.UserAgent = userAgent;
+            HttpWebResponse myrp = (HttpWebResponse)mywr.GetResponse();
+            if (myrp.StatusCode != HttpStatusCode.OK)
+            {
+                return null;
+            }
+            return myrp.GetResponseStream();
+        }
+    }
+}
