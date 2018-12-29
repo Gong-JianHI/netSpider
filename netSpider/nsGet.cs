3826a4d40989a384f4fd4d02d8423a7b69156147
Gong-JianHI <1365654168@qq.com>
2018-12-29 22:40:37 +0800

    代码封装
    整理了原来的代码，让其变得更加人性化

---
+++ netSpider/nsGet.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Administrator
+ * Date: 2018/12/29 星期六
+ * Time: 22:18
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+
+namespace netSpider.Properties
+{
+	/// <summary>
+	/// Description of nsGet.
+	/// </summary>
+	class nsGet
+	{
+		private static netSpiderBase nsb=new netSpiderBase();
+		public nsGet()
+		{
+		}
+		public static void getStream(nsManager nsm,string url){
+			HttpWebResponse hwp=nsb.getResponse(url,nsm.Keepalive,nsm.Timeout,nsm.Method,nsm.Accept,nsm.UserAgent);
+			nsm.streamh=hwp.GetResponseStream();
+			return;
+		}
+		public static HttpWebResponse getResponse(nsManager nsm,string url){
+			HttpWebResponse hwp=nsb.getResponse(url,nsm.Keepalive,nsm.Timeout,nsm.Method,nsm.Accept,nsm.UserAgent);
+			return hwp;
+		}
+	}
+}
---
+++ netSpider/nsManager.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Administrator
+ * Date: 2018/12/29 星期六
+ * Time: 22:01
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace netSpider
+{
+	/// <summary>
+	/// Description of nsManager.
+	/// </summary>
+	class nsManager
+	{
+		public nsManager()
+		{
+			
+		}
+		private string userAgent;
+		private string accept;
+		private string method;
+		private int timeout;
+		private bool keepalive;
+		public Stream streamh;
+		public bool Keepalive {
+			get {
+				return keepalive;
+			}
+			set{
+				keepalive=value;
+			}
+		}
+
+		public string UserAgent {
+			get {
+				return userAgent;
+			}
+			set{
+				accept=value;
+			}
+		}
+		public string Accept {
+			get {
+				return accept;
+			}
+			set{
+				userAgent=value;
+			}
+		}
+
+		public string Method {
+			get {
+				return method;
+			}
+			set{
+				method=value;
+			}
+		}
+		
+		public int Timeout {
+			get {
+				return timeout;
+			}
+			set{
+				timeout =value;
+			}
+		}
+		public string getString(){
+			string temp;
+			StreamReader s=new StreamReader (streamh);
+			temp=s.ReadToEnd();
+			return temp;
+		}
+	}
+}
